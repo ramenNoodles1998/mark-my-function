ea0c78c1dda50779af4537f115da34885e819a92
ramenNoodles1998 <romanmeredith@gmail.com>
2024-04-10 23:20:03 -0600

    enter //helllo in file.

--- mark-my-function/src/extension.ts
+++ mark-my-function/src/extension.ts
@@ -8,8 +8,6 @@ export function activate(context: vscode.ExtensionContext) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "mark-my-function" is now active!');
-
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
@@ -19,7 +17,10 @@ export function activate(context: vscode.ExtensionContext) {
 		const editor = vscode.window.activeTextEditor;
 		if (editor) {
 			const document = await vscode.workspace.openTextDocument(editor.document.uri);
-			let text = document.getText();
+			editor.edit(editBuilder => {
+				editBuilder.insert(new vscode.Position(33, 0), '\n//Hello\n');
+			});
+			let text = document.getText().split('\n');
 			console.log(text);
 		} else {
 			//throw error.
